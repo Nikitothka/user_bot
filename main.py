b396f723e0ec9f11dd485cac673d655275130d48
NIKITA <nik2003200314@mail.ru>
2022-02-04 01:58:46 +0300

    изучаю pyrogram

--- main.py
+++ main.py
@@ -9,94 +9,9 @@ import random
 
 app = Client("my_account")
 
-
-# Команда type
-@app.on_message(filters.command("type", prefixes=".") & filters.me)
-def type(_, msg):
-    orig_text = msg.text.split(".type ", maxsplit=1)[1]
-    text = orig_text
-    tbp = ""  # to be printed
-    typing_symbol = "▒"
-
-    while (tbp != orig_text):
-        try:
-            msg.edit(tbp + typing_symbol)
-            sleep(0.05)  # 50 ms
-
-            tbp = tbp + text[0]
-            text = text[1:]
-
-            msg.edit(tbp)
-            sleep(0.05)
-
-        except FloodWait as e:
-            sleep(e.x)
-
-
-# Команда взлома пентагона
-@app.on_message(filters.command("hack", prefixes=".") & filters.me)
-def hack(_, msg):
-    perc = 0
-
-    while (perc < 100):
-        try:
-            text = ":heart: :heart: " + str(perc) + "%"
-            msg.edit(text)
-
-            perc += random.randint(1, 3)
-            sleep(0.1)
-
-        except FloodWait as e:
-            sleep(e.x)
-
-    msg.edit("🟢 Пентагон успешно взломан!")
-    sleep(3)
-
-    msg.edit("👽 Поиск секретных данных об НЛО ...")
-    perc = 0
-
-    while (perc < 100):
-        try:
-            text = "👽 Поиск секретных данных об НЛО ..." + str(perc) + "%"
-            msg.edit(text)
-
-            perc += random.randint(1, 5)
-            sleep(0.15)
-
-        except FloodWait as e:
-            sleep(e.x)
-
-    msg.edit("🦖 Найдены данные о существовании динозавров на земле!")
-'''
-
-@app.on_message(filters.command("thanos", prefixes=".") & filters.me)
-def thanos(_, msg):
-    chat = msg.text.split(".thanos ", maxsplit=1)[1]
-
-    members = [
-        x
-        for x in app.iter_chat_members(chat)
-        if x.status not in ("administrator", "creator")
-    ]
-
-    random.shuffle(members)
-
-    app.send_message(chat, "Щелчок Таноса ... *щёлк*")
-
-    for i in range(len(members) // 2):
-        try:
-            app.restrict_chat_member(
-                chat_id=chat,
-                user_id=members[i].user.id,
-                permissions=ChatPermissions(),
-                until_date=int(time.time() + 86400),
-            )
-            app.send_message(chat, "Исчез " + members[i].user.first_name)
-        except FloodWait as e:
-            print("> waiting", e.x, "seconds.")
-            time.sleep(e.x)
-
-    app.send_message(chat, "Но какой ценой?")
-'''
+async def main():
+    await app.start()
+    await app.send_message("me", "Hi!")
+    await app.stop(
 
 app.run()
