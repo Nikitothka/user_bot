6dfa1a232e3d76546a187eb84581256d24672b0b
NIKITA <nik2003200314@mail.ru>
2022-02-04 21:35:23 +0300

    a function that makes line substitutions

---
+++ scrips_for_main/replase.py
@@ -0,0 +1,12 @@
+from addons.efict_run_line import Rename_text
+
+def Rep(cnt_string):
+    text = ''
+    for i in range(cnt_string):
+        text += Rename_text()
+        text +='\n'
+    return text
+
+
+
+
