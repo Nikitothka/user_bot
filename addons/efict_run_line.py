37514d81d238acffc016f8c444d44cb3f81854b1
NIKITA <nik2003200314@mail.ru>
2022-02-04 17:13:46 +0300

    create function string 13 element

--- addons/efict_run_line.py
+++ addons/efict_run_line.py
@@ -3,4 +3,11 @@ from addons.stickers_All import stickers_all as stick
 
 def Run_line():
     add_text= choice(stick.All)
+
     return add_text
+def Rename_text():
+    s = ''
+    for i in range(13):
+        s += choice(stick.All)
+    return s
+print(Rename_text())
