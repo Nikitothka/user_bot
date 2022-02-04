db95e27aa08cf7f6dcc4c4ea97f2b04971da7f1a
NIKITA <nik2003200314@mail.ru>
2022-02-04 17:12:20 +0300

    animation run string(7 second)

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from pyrogram.errors import FloodWait
 from addons import heart_ad
 from addons import stickers_All
 
-from pyrogram.types import ChatPermissions
+from addons import efict_run_line
 
+from pyrogram.types import ChatPermissions
 
 import time
 from time import sleep
@@ -19,11 +20,12 @@ def echo(client, message):
     app.send_message("me", ":heart:")
 '''
 
+
 @app.on_message(filters.command("heart", prefixes=".") & filters.me)
 def type(_, msg, heart, addons=None):
     orig_text = msg.text.split(".heart ", maxsplit=1)[1]
     print(orig_text)
-    #count = int(orig_text)
+    # count = int(orig_text)
     tbp = ""  # to be printed
     heart = ''
     heart = addons.heart_ad.Heart(heart)
@@ -40,28 +42,29 @@ def type(_, msg, heart, addons=None):
     #                 '🔥🔥🔥🔥🔥🔥💓🔥🔥🔥🔥🔥🔥' + '\n' \
     #                 '🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥' + '\n'
     #     #"💓💓"
-    cnt_1 =0
+    cnt_1 = 0
     text = ''
     line = 0
-    while line<10:
+    while line < 143:
         try:
-            #text =  #мэйн текс
-            msg.edit(text + heart.split()[line])
-            text = text + heart.split()[line]
+            if cnt_1 < 13:
+                msg.edit(text + efict_run_line.Run_line())
+                text = text + efict_run_line.Run_line()  # heart.split()[line]
+                cnt_1 += 1
+            else:
+                cnt_1 = 0
+                text = efict_run_line.Rename_text()
+                msg.edit(text + '\n')
             sleep(0.05)
-            cnt_1 +=1
-            line+=1
+            cnt_1 += 1
+            line += 1
         except FloodWait as e:
             sleep(e.x)
     try:
-        # text =  #мэйн текс
         msg.edit(heart)
-        text = text + heart
+        text = heart
     except FloodWait as e:
         sleep(e.x)
 
 
-
-
 app.run()
-
