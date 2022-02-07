72274188899c41abaf58df1aae0185ae8ccc19ae
NIKITA <nik2003200314@mail.ru>
2022-02-07 12:45:11 +0300

    beautiful and optimized animations (3)-version

--- main.py
+++ main.py
@@ -29,10 +29,12 @@ def type(_, msg):
     except:
         orig_text = 1
     print(orig_text)
+    heart_bool = True
     # count = int(orig_text)
     tbp = ""  # to be printed
     heart = ''
-    heart = addons.heart_ad.Heart(heart)
+    count= 0
+    #heart = addons.heart_ad.Heart(heart)
     # # heart =\
     #                 '🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥'+'\n' \
     #                 '🔥🔥🔥💓💓🔥🔥🔥💓💓🔥🔥🔥'+'\n' \
@@ -50,30 +52,50 @@ def type(_, msg):
     text = ''
     line = 0
     cnt_string = 0
-    while cnt_string < 3:
-        try:
-            if cnt_1 < 3:
-                msg.edit(text + efict_run_line.Run_line())
-                text = text + efict_run_line.Run_line()  # heart.split()[line]
-                cnt_1 += 1
-            else:
-                text_end = ''
-                cnt_1 = 0
-                cnt_string +=1
-                text = Rep(cnt_string)
-                text_end += text
-                msg.edit(text + '\n')
+    # while cnt_string < 3:
+    #     try:
+    #         if cnt_1 < 3:
+    #             msg.edit(text + efict_run_line.Run_line())
+    #             text = text + efict_run_line.Run_line()  # heart.split()[line]
+    #             cnt_1 += 1
+    #         else:
+    #             #text_end = ''
+    #             cnt_1 = 0
+    #             cnt_string +=1
+    #             text = Rep(cnt_string)
+    #             #text_end += text
+    #             msg.edit(text + '\n')
+    #
+    #         sleep(0.5)
+    #         cnt_1 += 1
+    #         line += 1
+    #     except FloodWait as e:
+    #         print(e.x)
+    #         sleep(e.x)
+    # print(heart_bool)
+    while count < 20:
 
-            sleep(0.5)
-            cnt_1 += 1
-            line += 1
-        except FloodWait as e:
-            sleep(e.x)
-    while True:
         try:
+            sleep(0.1)
             msg.edit(addons.heart_ad.Heart(heart))
+            sleep(0.5)
         except FloodWait as e:
+            print(e.x)
             sleep(e.x)
+        count +=1
+        print(count)
+@app.on_message(filters.command("stop", prefixes=".") & filters.me)
+def types(_, msg):
+    msg.edit('💩')
+    try:
+        print('False')
+        heart_bool = False
+        return heart_bool
+    except:
+        print(heart_bool)
 
 
-app.run()
+if __name__ == '__main__':
+    type
+    types
+    app.run()
